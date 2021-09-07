20cc12665f1d4461626170fe4390dd5a70c04855
Stephanehk <stephane@utexas.edu>
2021-09-07 15:40:04 -0500

    made suggested changes

--- prefrence_predicting/successor_feature_iterative.py
+++ prefrence_predicting/successor_feature_iterative.py
@@ -19,7 +19,7 @@ def value_iteration():
     # initialize Q
     # Q = defaultdict(lambda: np.zeros(env.action_space))
     V = np.zeros((int(np.sqrt(env.observation_space)), int(np.sqrt(env.observation_space))))
-    M = [[np.zeros(env.observation_space) for i in range(int(np.sqrt(env.observation_space)))] for j in range (int(np.sqrt(env.observation_space)))]
+    M = [[np.zeros(env.feature_size) for i in range(int(np.sqrt(env.observation_space)))] for j in range (int(np.sqrt(env.observation_space)))]
 
     actions = [[-1,0],[1,0],[0,-1],[0,1]]
     n = 0
@@ -49,9 +49,10 @@ def value_iteration():
         if delta < THETA:
             break
 
-    #----------------------------------------------------------------------------------------------------------------------------------------
-    #iterativley learn succesor features
-    #----------------------------------------------------------------------------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------------------------------------------------------------------------
+    # iterativley learn reward succesor features
+    # ----------------------------------------------------------------------------------------------------------------------------------------
     while True:
         #loop through every state
         delta = 0
@@ -60,83 +61,107 @@ def value_iteration():
                 if env.is_blocked(i,j):
                     continue
                 m_original = M[i][j]
-                s_tab,N = env.state2tab(i,j)
+                # s_tab,N = env.state2tab(i,j)
                 # print ("here")
                 state_Qs = []
                 state_Ms = []
                 total_occupancies = []
                 for a_index in range(len(actions)):
-                    next_state, reward, done, _ = env.get_next_state((i,j),a_index)
+                    next_state, reward, done, reward_feature = env.get_next_state((i,j),a_index)
                     # print (reward)
                     ni,nj = next_state
 
                     if not done:
                         Q = reward + GAMMA*V[ni][nj]
-                        m = s_tab + FGAMMA*M[ni][nj]
+                        m = reward_feature + FGAMMA*M[ni][nj]
                     else:
                         Q = reward
-                        m = s_tab
+                        # assert (sum(reward_feature) == 1)
+                        m = reward_feature
                     state_Qs.append(Q)
                     state_Ms.append(m)
 
+                #value iteration
                 M[i][j] = state_Ms[np.argmax(state_Qs)]
+                #policy evaluation
+                # M[i][j] = state_Ms[np.argmax(np.sum(state_Ms,axis=1))]
                 delta = max(delta,np.sum(np.abs((m_original-M[i][j]))))
 
         if delta < THETA:
             break
 
-
-    # # checks that the reward array*successor features == state value
-    # for i in range (10):
-    #     for j in range (10):
-    #         m = M[i][j]
-    #         V_pred = 0
-    #         # V_pred = np.dot(m,env.reward_array)
-    #         # V_pred -= np.abs(env.reward_array[i + 10*j]) #make sure we do not give award for start state
-    #         # print ((i,j))
-    #         # print (V_pred - V[i][j])
-    #         # # print (V_pred)
-    #         # print (m)
-    #         last_pos = None
-    #         for n in range(len(m)):
-    #             if m[n] == 1:
-    #                 # print(env.reward_array[n])
-    #                 x = n % 10
-    #                 y = n // 10
-    #                 if last_pos != None:
-    #                     a = [x-last_pos[0],y-last_pos[1]]
-    #                     a_index = env.find_action_index(a)
-    #                     next_state, reward, done, _ = env.get_next_state((last_pos[0],last_pos[1]),a_index)
-    #                     V_pred += reward
+    #----------------------------------------------------------------------------------------------------------------------------------------
+    #iterativley learn state succesor features
+    #----------------------------------------------------------------------------------------------------------------------------------------
+    # while True:
+    #     #loop through every state
+    #     delta = 0
+    #     for i in range (10):
+    #         for j in range (10):
+    #             if env.is_blocked(i,j):
+    #                 continue
+    #             m_original = M[i][j]
+    #             s_tab,N = env.state2tab(i,j)
+    #             # print ("here")
+    #             state_Qs = []
+    #             state_Ms = []
+    #             total_occupancies = []
+    #             for a_index in range(len(actions)):
+    #                 next_state, reward, done, _ = env.get_next_state((i,j),a_index)
+    #                 # print (reward)
+    #                 ni,nj = next_state
     #
-    #                 last_pos =[x,y]
+    #                 if not done:
+    #                     Q = reward + GAMMA*V[ni][nj]
+    #                     m = s_tab + FGAMMA*M[ni][nj]
+    #                 else:
+    #                     Q = reward
+    #                     m = s_tab
+    #                 state_Qs.append(Q)
+    #                 state_Ms.append(m)
+    #
+    #             #value iteration
+    #             M[i][j] = state_Ms[np.argmax(state_Qs)]
+    #             #policy evaluation
+    #             # M[i][j] = state_Ms[np.argmax(np.sum(state_Ms,axis=1))]
+    #             delta = max(delta,np.sum(np.abs((m_original-M[i][j]))))
+    #
+    #     if delta < THETA:
+    #         break
+    #----------------------------------------------------------------------------------------------------------------------------------------
+    #check succesor features
+    #----------------------------------------------------------------------------------------------------------------------------------------
 
-            # # print ("\n")
-            # if V[i][j] != V_pred:
-            #     last_pos = None
-            #     for n in range(len(m)):
-            #         if m[n] == 1:
-            #             # print(env.reward_array[n])
-            #             x = n % 10
-            #             y = n // 10
-            #             print ((x,y))
-            #             if last_pos != None:
-            #                 a = [x-last_pos[0],y-last_pos[1]]
-            #                 a_index = env.find_action_index(a)
-            #                 next_state, reward, done, _ = env.get_next_state((last_pos[0],last_pos[1]),a_index)
-            #                 print (reward)
-            #                 # V_pred += reward
-            #
-            #             last_pos =[x,y]
-            #             print ("\n")
-            #
-            #     print ("\n\n")
-            #     print (V[i][j])
-            #     print (V_pred)
-            #     print ("\n")
-            #     assert False
-            # assert (V[i][j] == V_pred)
+    #subtract inital state reward
+    for i in range (10):
+        for j in range (10):
+            state_Qs = []
+            for a_index in range(len(actions)):
+                next_state, reward, done, reward_feature = env.get_next_state((i,j),a_index)
+                if not done:
+                    Q = reward + GAMMA*V[ni][nj]
+                else:
+                    Q = reward
+                state_Qs.append(Q)
 
+            next_state, reward, done, reward_feature = env.get_next_state((i,j),np.argmax(state_Qs))
+            M[i][j] -= reward_feature
+
+    #test code
+    for i in range (10):
+        for j in range (10):
+            if env.is_blocked(i,j):
+                continue
+            m = M[i][j]
+            v_pred = np.dot(m,env.reward_array)
+            # print ((i,j))
+            # print (m)
+            # print (v_pred)
+            # print (V[i][j])
+            # print ("\n")
+            assert (v_pred == V[i][j])
+
+    print (M[0][0])
 
 # Q = np.load("/Users/stephanehatgiskessell/Desktop/Kivy_stuff/boards/2021-07-29_sparseboard2-notrap_Qs.npy")
 value_iteration()
